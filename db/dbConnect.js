b193ea40653da550d5ba5b325b541d579de5506b
Zaco <cyrilmathe08@gmail.com>
2025-03-14 14:02:06 +0100

    :sparkles: ajout de la bdd et de la fonction pour la connecter

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,1 +1,14 @@
 // connecter la db
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB)
+    console.log(`MongoDB connecté`);
+  } catch (error) {
+    console.error(` Problème lors de la connection`, error);
+    process.exit(1)
+  }
+}
+
+export default connectDB
