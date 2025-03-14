da4825c6c9897b0df1b8a6b0fe407c7c2aee2f33
Imane Iguederzen <imane.iguederzen76@gmail.com>
2025-03-14 14:42:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // front
 import express from 'express'
+import { Server } from 'socket.io';
+import cors from 'cors'
+
+const app = express();
+const port = 8080
+
+app.use(cors('http://localhost:3000'))
+
+const server = app.listen(port, () => {
+    console.log(`Le serveur écoute sur le port ${port}`);
+})
+
+const io = new Server (server, {
+    cors: {
+        origin: 'http://localhost:3000', 
+        methods: ['GET', 'POST'],
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Un client est connecté');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un client est déconnecté');
+    });
+});
